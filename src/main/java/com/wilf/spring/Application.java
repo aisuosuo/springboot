7947ea03ae87e836555bc1d6feaf27f6629032ee
wilf <wilf@txzing.com>
2020-01-19 18:21:06 +0800

    first commit

---
+++ src/main/java/com/wilf/spring/Application.java
@@ -0,0 +1,14 @@
+package com.wilf.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangfeng
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/wilf/spring/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.wilf.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangfeng
+ */
+@RestController
+public class HelloWorldController {
+    @RequestMapping("hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
